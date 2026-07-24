9d74b96204fe6be7a9359390d16e57ddd89ae9bf
mohsafaei <130284531+mohsafaei@users.noreply.github.com>
2026-07-24 15:59:53 +0000

    Deploying to gh-pages from @ samirasadeghi1992/samirasadeghi1992.github.io@8a28136bbfb34379d662ab5176fcb588310d7b64 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,20 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "Projects",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
-          title: "Repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "",
